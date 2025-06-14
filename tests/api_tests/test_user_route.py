d57be5d74eaf44f4ea67e606ab2e19d2ce37b701
Tomas Tamantini <tomasbft@gmail.com>
2025-06-14 14:50:00 -0300

    Test request with missing fields

--- tests/api_tests/test_user_route.py
+++ tests/api_tests/test_user_route.py
@@ -1,16 +1,15 @@
 from http import HTTPStatus
 
 
-def test_posting_valid_user_returns_status_created(
-    client, valid_create_user_request
-):
-    response = client.post("/users", json=valid_create_user_request)
-    assert response.status_code == HTTPStatus.CREATED
-
-
-def test_posting_user_with_invalid_email_returns_status_unprocessable_entity(
+def test_creating_user_with_missing_fields_returns_unprocessable_entity(
     client,
-    invalid_email_create_user_request,
+):
+    response = client.post("/users", json={})
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+def test_creating_user_with_invalid_email_returns_status_unprocessable_entity(
+    client, invalid_email_create_user_request
 ):
     response = client.post("/users", json=invalid_email_create_user_request)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
