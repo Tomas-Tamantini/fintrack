eee7bba11ec2b545e55dbe1c27b586473c8f575f
Tomas Tamantini <tomasbft@gmail.com>
2025-06-14 12:27:34 -0300

    Extract client fixtures

--- tests/api_tests/test_user_route.py
+++ tests/api_tests/test_user_route.py
@@ -1,11 +1,6 @@
 from http import HTTPStatus
 
 import pytest
-from fastapi import testclient
-
-from fintrack.app import app
-
-client = testclient.TestClient(app)
 
 
 @pytest.fixture
@@ -26,18 +21,23 @@ def invalid_email_create_user_request():
     }
 
 
-def test_posting_valid_user_returns_status_created(valid_create_user_request):
+def test_posting_valid_user_returns_status_created(
+    client, valid_create_user_request
+):
     response = client.post("/users", json=valid_create_user_request)
     assert response.status_code == HTTPStatus.CREATED
 
 
 def test_posting_user_with_invalid_email_returns_status_unprocessable_entity(
+    client,
     invalid_email_create_user_request,
 ):
     response = client.post("/users", json=invalid_email_create_user_request)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
-def test_created_user_is_returned_without_password(valid_create_user_request):
+def test_created_user_is_returned_without_password(
+    client, valid_create_user_request
+):
     response = client.post("/users", json=valid_create_user_request)
     assert "password" not in response.json()
