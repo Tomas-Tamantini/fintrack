949183ac5846c9d380e4a9d04cb788465ce10c15
Tomas Tamantini <tomasbft@gmail.com>
2025-06-18 16:24:06 -0300

    Hash password with pwdlib

--- fintrack/api/dependencies/authentication.py
+++ fintrack/api/dependencies/authentication.py
@@ -1,12 +1,13 @@
 from typing import Annotated
 
 from fastapi import Depends
+from pwdlib import PasswordHash
 
 from fintrack.api.authentication.password_handler import PasswordHandler
 
 
 def get_password_handler() -> PasswordHandler:
-    raise NotImplementedError()  # TODO: Implement
+    return PasswordHash.recommended()
 
 
 T_PasswordHandler = Annotated[PasswordHandler, Depends(get_password_handler)]
